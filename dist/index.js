7225576093cf7c8acab383eb2f2f217dc2f84822
MacPro Josh <josh@MacPros-MacBook-Pro.local>
2021-04-02 18:26:39 -0700

    add colors

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var images_1 = require("./images");
+var colors_1 = require("./colors");
 module.exports = {
     avatars: images_1.image_avatars,
+    colors: colors_1._colors,
     default: images_1.image_avatars,
 };
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { image_avatars} from "./images";
+import { _colors } from "./colors";
 module.exports = {
     avatars : image_avatars,
+    colors: _colors,
     default: image_avatars,
 }
